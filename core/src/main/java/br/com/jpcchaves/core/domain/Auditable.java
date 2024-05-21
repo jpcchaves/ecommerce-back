d83a266806077bda5c38cc6823ddb1ecd06e276a
jpcchaves <joao.chaves@liferay.com>
2024-05-21 09:07:36 -0300

    add auditable core entity

---
+++ core/src/main/java/br/com/jpcchaves/core/domain/Auditable.java
@@ -0,0 +1,63 @@
+package br.com.jpcchaves.core.domain;
+
+import java.time.LocalDateTime;
+
+public abstract class Auditable<PK> {
+  protected PK createdBy;
+  protected PK lastModifiedBy;
+  protected LocalDateTime createdDate;
+  protected LocalDateTime lastModifiedDate;
+
+  public Auditable() {
+  }
+
+  public Auditable(LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
+    this.createdDate = createdDate;
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public Auditable(PK lastModifiedBy, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
+    this.lastModifiedBy = lastModifiedBy;
+    this.createdDate = createdDate;
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public Auditable(PK createdBy, PK lastModifiedBy, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
+    this.createdBy = createdBy;
+    this.lastModifiedBy = lastModifiedBy;
+    this.createdDate = createdDate;
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public PK getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(PK createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public PK getLastModifiedBy() {
+    return lastModifiedBy;
+  }
+
+  public void setLastModifiedBy(PK lastModifiedBy) {
+    this.lastModifiedBy = lastModifiedBy;
+  }
+
+  public LocalDateTime getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(LocalDateTime createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public LocalDateTime getLastmodifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public void setLastmodifiedDate(LocalDateTime lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+}
