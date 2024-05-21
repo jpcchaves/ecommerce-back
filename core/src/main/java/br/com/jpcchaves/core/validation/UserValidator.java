583000101a0b963a6c45d1f0bc2ff0a0ad9eeb11
jpcchaves <joao.chaves@liferay.com>
2024-05-21 11:56:33 -0300

    add user validator

---
+++ core/src/main/java/br/com/jpcchaves/core/validation/UserValidator.java
@@ -0,0 +1,48 @@
+package br.com.jpcchaves.core.validation;
+
+import br.com.jpcchaves.core.domain.User;
+import br.com.jpcchaves.core.exception.UserException;
+import br.com.jpcchaves.core.exception.enums.ExceptionDefinition;
+
+public class UserValidator implements Validator<User> {
+  private static final int MAX_LENGTH = 50;
+  private static final int MIN_PASSWORD_LENGTH = 8;
+
+
+  @Override
+  public void validate(User entity) {
+    if (entity == null) {
+      throw new UserException(ExceptionDefinition.USR0004);
+    }
+
+    validateNotBlank(entity.getUsername());
+    validateNotBlank(entity.getEmail());
+    validateNotBlank(entity.getFirstName());
+    validateNotBlank(entity.getLastName());
+    validateNotBlank(entity.getPassword());
+
+    validateMaxLength(entity.getUsername(), MAX_LENGTH);
+    validateMaxLength(entity.getFirstName(), MAX_LENGTH);
+    validateMaxLength(entity.getLastName(), MAX_LENGTH);
+
+    if (!EmailValidator.isValid(entity.getEmail())) {
+      throw new UserException(ExceptionDefinition.GEN0001);
+    }
+
+    if (entity.getPassword().length() < MIN_PASSWORD_LENGTH) {
+      throw new UserException(ExceptionDefinition.USR0003);
+    }
+  }
+
+  private void validateNotBlank(String value) {
+    if (value == null || value.isBlank()) {
+      throw new UserException(ExceptionDefinition.USR0001);
+    }
+  }
+
+  private void validateMaxLength(String value, int maxLength) {
+    if (value.length() > maxLength) {
+      throw new UserException(ExceptionDefinition.USR0001);
+    }
+  }
+}
