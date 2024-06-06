028c717c1df11c276f4fc7fdd0c33d73701b996e
Joao Chaves <joao.chaves@liferay.com>
2024-06-06 11:07:49 -0300

    add role attribute and fromString method

--- core/src/main/java/com/ecommerce/core/model/Enum/UserRole.java
+++ core/src/main/java/com/ecommerce/core/model/Enum/UserRole.java
@@ -1,6 +1,25 @@
 package com.ecommerce.core.model.Enum;
 
 public enum UserRole {
-  USER,
-  ADMIN
+  USER_ROLE("USER_ROLE"),
+  ADMIN_ROLE("ADMIN_ROLE");
+
+  private final String role;
+
+  UserRole(String role) {
+    this.role = role;
+  }
+
+  public String fromString(String role) {
+    for (UserRole userRole : values()) {
+
+      if (userRole.role.equals(role)) {
+
+        return role;
+      }
+
+    }
+
+    throw new IllegalArgumentException("");
+  }
 }
