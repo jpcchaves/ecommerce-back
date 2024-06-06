6315416e06611d4226c1d54f7dc59e3a00651e17
Joao Chaves <joao.chaves@liferay.com>
2024-06-06 14:04:52 -0300

    add message response dto and user register usecase interface

---
+++ core/src/main/java/com/ecommerce/core/dto/common/MessageResponseDTO.java
@@ -0,0 +1,28 @@
+package com.ecommerce.core.dto.common;
+
+public class MessageResponseDTO {
+    private String message;
+
+    private MessageResponseDTO(String message) {
+        this.message = message;
+    }
+
+    public static MessageResponseDTO message(String message) {
+        return new MessageResponseDTO(message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageResponseDTO{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
