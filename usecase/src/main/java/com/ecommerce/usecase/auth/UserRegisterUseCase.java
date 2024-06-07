6622a04aee4a408b17c30c768447b2d24a1691fe
João Chaves <joao.chaves@liferay.com>
2024-06-06 21:16:11 -0300

    remove old jar and add prettier java plugin

--- usecase/src/main/java/com/ecommerce/usecase/auth/UserRegisterUseCase.java
+++ usecase/src/main/java/com/ecommerce/usecase/auth/UserRegisterUseCase.java
@@ -4,5 +4,5 @@ import com.ecommerce.core.dto.auth.UserRegisterDTO;
 import com.ecommerce.core.dto.common.MessageResponseDTO;
 
 public interface UserRegisterUseCase {
-  MessageResponseDTO register(UserRegisterDTO request);
+    MessageResponseDTO register(UserRegisterDTO request);
 }
