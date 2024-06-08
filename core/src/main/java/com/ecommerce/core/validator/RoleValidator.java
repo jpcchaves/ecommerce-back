3f459703430083fc2509013667fcd8e003c7b523
João Chaves <joao.chaves@liferay.com>
2024-06-08 20:41:19 -0300

    apply singleton pattern in role validator

--- core/src/main/java/com/ecommerce/core/validator/RoleValidator.java
+++ core/src/main/java/com/ecommerce/core/validator/RoleValidator.java
@@ -6,6 +6,18 @@ import com.ecommerce.core.model.Role;
 
 public class RoleValidator implements Validator<Role> {
 
+  private static RoleValidator INSTANCE;
+
+  private RoleValidator() {}
+
+  public static RoleValidator getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new RoleValidator();
+    }
+
+    return INSTANCE;
+  }
+
   @Override
   public void validate(Role entity) {
     if (entity.getName().isBlank()) {
