27754e10fd1f5b594c245e1efbda915f91abb66d
João Chaves <joao.chaves@liferay.com>
2024-06-08 20:39:19 -0300

    apply singleton pattern in ProductValidator

--- core/src/main/java/com/ecommerce/core/validator/ProductValidator.java
+++ core/src/main/java/com/ecommerce/core/validator/ProductValidator.java
@@ -5,6 +5,18 @@ import com.ecommerce.core.validator.util.ValidationUtil;
 
 public class ProductValidator implements Validator<Product> {
 
+  private static ProductValidator INSTANCE = new ProductValidator();
+
+  private ProductValidator() {}
+
+  public static ProductValidator getInstance() {
+    if (INSTANCE == null) {
+      INSTANCE = new ProductValidator();
+    }
+
+    return INSTANCE;
+  }
+
   @Override
   public void validate(Product product) {
     ValidationUtil.notBlank(product.getName(), "name");
