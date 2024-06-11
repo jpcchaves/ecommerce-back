7e57a31be4a4f90ab3d85cdbdaa4842a74248269
Joao Chaves <joao.chaves@liferay.com>
2024-06-11 08:10:28 -0300

    decouple the validator class from it's impl

--- application/src/main/java/com/cleanarch/application/auth/usecase/LoginUseCaseImpl.java
+++ application/src/main/java/com/cleanarch/application/auth/usecase/LoginUseCaseImpl.java
@@ -4,20 +4,22 @@ import com.cleanarch.application.auth.gateway.LoginGateway;
 import com.ecommerce.core.dto.auth.LoginRequestDTO;
 import com.ecommerce.core.dto.auth.LoginResponseDTO;
 import com.ecommerce.core.validator.LoginValidator;
+import com.ecommerce.core.validator.Validator;
 import com.ecommerce.usecase.auth.LoginUseCase;
 
 public class LoginUseCaseImpl implements LoginUseCase {
 
-  private final LoginGateway loginGateway;
+    private final LoginGateway loginGateway;
+    private final Validator<LoginRequestDTO> validator = LoginValidator.getInstance();
 
-  public LoginUseCaseImpl(LoginGateway loginGateway) {
-    this.loginGateway = loginGateway;
-  }
+    public LoginUseCaseImpl(LoginGateway loginGateway) {
+        this.loginGateway = loginGateway;
+    }
 
-  @Override
-  public LoginResponseDTO login(LoginRequestDTO request) {
-    LoginValidator.getInstance().validate(request);
+    @Override
+    public LoginResponseDTO login(LoginRequestDTO request) {
+        validator.validate(request);
 
-    return loginGateway.login(request);
-  }
+        return loginGateway.login(request);
+    }
 }
